1303ec0fcef5d3c6ad0b3f704f77b6ba80422a51
Hector Merla <hectormerla@Hectors-MacBook-Air.local>
2018-08-06 18:10:53 -0500

    Fix dist main file

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const HtmlWebPackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  entry: './dev/index.js',
+  entry: './src/index.js',
   module: {
     rules: [
       {
